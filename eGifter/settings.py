a4e034de338378fc536bd0d1307a442ca9b23ab8
iammadhanraj <madhanreigns312@gmail.com>
2024-06-28 00:40:17 +0530

    added models in gifts app

--- eGifter/settings.py
+++ eGifter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'gifts'
+    'gifts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "eGifter.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Gifts(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4,unique=True,editable=False)
+    creator=models.CharField(max_length=255,blank=False,null=False)
+    img=models.ImageField(upload_to='gifts/images')
+    description=models.TextField(max_length=500)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='Gift'
+        verbose_name_plural='Gifts'
+        
+    def __str__(self):
+        return str(self.id)
