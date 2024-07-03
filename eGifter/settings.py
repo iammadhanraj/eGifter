8264b11b1e04574d9804b8f53b8517d9970dc178
iammadhanraj <madhanreigns312@gmail.com>
2024-07-03 22:50:54 +0530

    made changes

--- eGifter/settings.py
+++ eGifter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'gifts',
     # 'ckeditor',
 ]
